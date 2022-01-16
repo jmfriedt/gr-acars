30c0f995b2d1acb90b94e3b165139ba19ea79b6b
Jean-Michel Friedt <friedtj@free.fr>
2022-01-16 07:58:16 +0100

    RPi4 example update with Western Europen frequencies

--- 3.9ng/examples/example.py
+++ 3.9ng/examples/example.py
@@ -8,26 +8,9 @@
 # Title: Not titled yet
 # GNU Radio version: 3.9.4.0
 
-from distutils.version import StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
-
-from PyQt5 import Qt
-from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
 from gnuradio import analog
-from gnuradio import blocks
-import pmt
 from gnuradio import filter
+from gnuradio.filter import firdes
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
@@ -36,43 +19,16 @@ from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 import acars
+import osmosdr
+import time
 
 
 
-from gnuradio import qtgui
 
-class example(gr.top_block, Qt.QWidget):
+class example(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "example")
-
-        try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
 
         ##################################################
         # Variables
@@ -84,48 +40,21 @@ class example(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
-            1024, #size
-            window.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
-            samp_rate, #bw
-            "", #name
-            1,
-            None # parent
+        self.osmosdr_source_0 = osmosdr.source(
+            args="numchan=" + str(1) + " " + ""
         )
-        self.qtgui_freq_sink_x_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
-        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0.enable_control_panel(False)
-        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
-
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.osmosdr_source_0.set_sample_rate(samp_rate)
+        self.osmosdr_source_0.set_center_freq(131.725e6, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(40, 0)
+        self.osmosdr_source_0.set_if_gain(20, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna('', 0)
+        self.osmosdr_source_0.set_bandwidth(0, 0)
         self.low_pass_filter_0 = filter.fir_filter_ccf(
             N,
             firdes.low_pass(
@@ -135,18 +64,22 @@ class example(gr.top_block, Qt.QWidget):
                 samp_rate/N/2,
                 window.WIN_HAMMING,
                 6.76))
-        self.freq_xlating_fir_filter_xxx_0_0 = filter.freq_xlating_fir_filter_ccc(N, taps, -100000, samp_rate)
-        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(N, taps, 200000, samp_rate)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '../../3.8ng/examples/gr-acars_1.152M_2.bin', False, 0, 0)
-        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.analog_am_demod_cf_0_1 = analog.am_demod_cf(
+        self.freq_xlating_fir_filter_xxx_0_1 = filter.freq_xlating_fir_filter_ccc(N, taps, 200000, samp_rate)
+        self.freq_xlating_fir_filter_xxx_0_0 = filter.freq_xlating_fir_filter_ccc(N, taps, -125000, samp_rate)
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(N, taps, 200000-25000, samp_rate)
+        self.analog_am_demod_cf_3 = analog.am_demod_cf(
         	channel_rate=samp_rate/N,
         	audio_decim=int(24/N),
         	audio_pass=5000,
         	audio_stop=5500,
         )
-        self.analog_am_demod_cf_0_0 = analog.am_demod_cf(
+        self.analog_am_demod_cf_2 = analog.am_demod_cf(
+        	channel_rate=samp_rate/N,
+        	audio_decim=int(24/N),
+        	audio_pass=5000,
+        	audio_stop=5500,
+        )
+        self.analog_am_demod_cf_1 = analog.am_demod_cf(
         	channel_rate=samp_rate/N,
         	audio_decim=int(24/N),
         	audio_pass=5000,
@@ -158,8 +91,9 @@ class example(gr.top_block, Qt.QWidget):
         	audio_pass=5000,
         	audio_stop=5500,
         )
-        self.acars_acars_0_1 = acars.acars(3, '/tmp/logm100', False)
-        self.acars_acars_0_0 = acars.acars(3, '/tmp/logp200', False)
+        self.acars_acars_3 = acars.acars(3, '/tmp/logm125', False)
+        self.acars_acars_2 = acars.acars(3, '/tmp/logp175', False)
+        self.acars_acars_1 = acars.acars(3, '/tmp/logp200', False)
         self.acars_acars_0 = acars.acars(3, '/tmp/log0', False)
 
 
@@ -167,25 +101,18 @@ class example(gr.top_block, Qt.QWidget):
         # Connections
         ##################################################
         self.connect((self.analog_am_demod_cf_0, 0), (self.acars_acars_0, 0))
-        self.connect((self.analog_am_demod_cf_0_0, 0), (self.acars_acars_0_1, 0))
-        self.connect((self.analog_am_demod_cf_0_1, 0), (self.acars_acars_0_0, 0))
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.freq_xlating_fir_filter_xxx_0_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.analog_am_demod_cf_0_1, 0))
-        self.connect((self.freq_xlating_fir_filter_xxx_0_0, 0), (self.analog_am_demod_cf_0_0, 0))
+        self.connect((self.analog_am_demod_cf_1, 0), (self.acars_acars_1, 0))
+        self.connect((self.analog_am_demod_cf_2, 0), (self.acars_acars_2, 0))
+        self.connect((self.analog_am_demod_cf_3, 0), (self.acars_acars_3, 0))
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.analog_am_demod_cf_2, 0))
+        self.connect((self.freq_xlating_fir_filter_xxx_0_0, 0), (self.analog_am_demod_cf_3, 0))
+        self.connect((self.freq_xlating_fir_filter_xxx_0_1, 0), (self.analog_am_demod_cf_1, 0))
         self.connect((self.low_pass_filter_0, 0), (self.analog_am_demod_cf_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.freq_xlating_fir_filter_xxx_0_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.freq_xlating_fir_filter_xxx_0_1, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.low_pass_filter_0, 0))
 
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "example")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -193,9 +120,8 @@ class example(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.set_taps(firdes.low_pass(1.0, self.samp_rate, self.samp_rate/self.N/2, self.samp_rate/self.N/2, window.WIN_HAMMING, 6.76))
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.samp_rate/self.N/2, self.samp_rate/self.N/2, window.WIN_HAMMING, 6.76))
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
 
     def get_N(self):
         return self.N
@@ -212,37 +138,32 @@ class example(gr.top_block, Qt.QWidget):
         self.taps = taps
         self.freq_xlating_fir_filter_xxx_0.set_taps(self.taps)
         self.freq_xlating_fir_filter_xxx_0_0.set_taps(self.taps)
+        self.freq_xlating_fir_filter_xxx_0_1.set_taps(self.taps)
 
 
 
 
 def main(top_block_cls=example, options=None):
-
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
-
-    tb.start()
-
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        Qt.QApplication.quit()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
 
-    qapp.exec_()
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
 
 if __name__ == '__main__':
     main()
