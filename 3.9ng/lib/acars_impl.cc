ab5a98f0fbfd0f6b811938c29b81a88c43be46d0
Jean-Michel Friedt <friedtj@free.fr>
2022-01-16 13:15:49 +0100

    libacars example + end of string

--- 3.9ng/lib/acars_impl.cc
+++ 3.9ng/lib/acars_impl.cc
@@ -14,6 +14,16 @@
 #include <gnuradio/fft/fft.h>
 #include <time.h>
 
+#ifdef LIBACARS
+#include <libacars/libacars.h>  // la_proto_node, la_proto_tree_destroy(),
+                                // la_proto_tree_format_text()
+#include <libacars/acars.h>     // la_acars_decode_apps(), la_acars_extract_sublabel_and_mfi()
+#include <libacars/vstring.h>   // la_vstring, la_vstring_destroy()
+#include <stdbool.h>            // bool
+#include <stdio.h>              // printf(3)
+#include <string.h>             // strlen(3)
+#endif
+
 #define fs        48000   // sampling frequency
 #define CHUNK_SIZE 1024   // minimum number of samples for trigger this processing block
 #define MESSAGE   (220*2) // twice the max message size !
@@ -169,6 +179,40 @@ void acars_impl::acars_parse(char *message,int ends)
                             } while ((k<ends-1) && (message[k-1]!=0x03));
                          printf("\n"); fprintf(_FILE, "\n");
                         }
+#ifdef LIBACARS
+	char *label = "H1";
+	char message[256];
+	char sublabel[3];
+	char mfi[3];
+	la_msg_dir direction = LA_MSG_DIR_AIR2GND;
+        message[k]=0; // for strlen
+
+	// The label is H1 which means the message text contains one or two additional fields -
+	// sublabel (int this case "M1") and Message Function Identifier ("B6"). These have to
+	// be stripped before calling la_acars_decode_apps(). la_acars_extract_sublabel_and_mfi()
+	// does this conveniently for us. It also copies these two fields to the given char buffers
+	// which must have a size of at least 3 bytes).
+	int offset = la_acars_extract_sublabel_and_mfi(label, direction, &message[22],
+			strlen(&message[28]), sublabel, mfi);
+	char *ptr = message;
+	// If the value returned by la_acars_extract_sublabel_and_mfi() is greater than 0, it means
+	// that at least the sublabel has been found. The value indicates how many bytes we need
+	// to skip over.
+	if(offset > 0) {
+		ptr += offset;
+	}
+	// Now look for supported ACARS application and decode it if found
+	la_proto_node *node = la_acars_decode_apps(label, ptr, direction);
+	if(node != NULL) {
+		la_vstring *vstr = la_proto_tree_format_text(NULL, node);
+		printf("Sublabel: %s MFI: %s\n", sublabel, mfi);
+		printf("Decoded message:\n%s\n", vstr->str);
+		fprintf(_FILE,"Sublabel: %s MFI: %s\n", sublabel, mfi);
+		fprintf(_FILE,"Decoded message:\n%s\n", vstr->str);
+		la_vstring_destroy(vstr, true);
+	}
+	la_proto_tree_destroy(node);
+#endif
                     }
                 }
             }
